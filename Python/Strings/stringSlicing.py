9eb95e5b19f90507acc499951f3d8424bca46d42
chukwuebuka alaegbu <chukwuebukaalaegbu@gmail.com>
2026-01-19 10:59:35 +0100

    slicing strings

--- Python/Strings/stringSlicing.py
+++ Python/Strings/stringSlicing.py
@@ -4,4 +4,12 @@
 value  = "Plasmodium"
 
 #getting the first 4 characters
-print(value[:5])  #Output: Data
+print(value[:4])  #Output: Data
+
+#getting last 4 characters
+print(value[-4:])
+
+#getting characters from index 2 to index 6
+print(value[2:7])  #Output: asmod
+
+print(value[-5:-2]) #Output: diu
