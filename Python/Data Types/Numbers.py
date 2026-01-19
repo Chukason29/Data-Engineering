24aa4c0aa216a3aef2b81efd4bb8a31307e77b7f
chukwuebuka alaegbu <chukwuebukaalaegbu@gmail.com>
2026-01-19 10:19:54 +0100

    strings

--- Python/Data Types/Numbers.py
+++ Python/Data Types/Numbers.py
@@ -1,3 +1,4 @@
+import random
 #Int Values
 a = 10
 b = -5
@@ -12,3 +13,16 @@ e = -2.5
 #You can cast int to float
 f = float(10)
 print(f)  # Output: 10.0
+
+#Complex Numbers
+g = complex(2, 3)  # 2 is the real part, 3 is the imaginary part
+h = complex(-1, -1)
+print(g)  # Output: (2+3j)
+print(h)  # Output: (-1-1j)
+
+#randrange is used to generate random numbers within a specified range
+randomNumber = random.randrange(1, 100)
+print(randomNumber)  # Output: Random number between 1 and 99
+
+
+#NB ==> You can also cast numbers into a string
---
+++ Python/Strings/strings.py
@@ -0,0 +1,7 @@
+#multiline string example
+multiline_string = """This is an example of a multiline string.
+It can span multiple lines.dsvfvfvsdfvdsfsfvsfvdsfv
+sdvfsdfvdfvsdfvdsfvdfv
+vsdfvsdfvsdfvdsfvsdfvsdfsdfvfvsdvfd
+"""
+print(multiline_string)
