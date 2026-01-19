aafba0ef34bdcd139518500b1f45c094fde70e0f
chukwuebuka alaegbu <chukwuebukaalaegbu@gmail.com>
2026-01-19 12:04:27 +0100

    accessing Lists

---
+++ Python/Lists/changingLists.py
@@ -0,0 +1,5 @@
+#You can replace elements in a list by specifying the index and assigning a new value.
+fruits = ["banana", "apple", "cherry", "date", "oranges","kiwi", "mango"]
+print(fruits[3]) #Prints date
+fruits[3] = "Guava"
+print(fruits[3]) #Prints Guava
