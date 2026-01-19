61c0aa9aff8e9ce9495df84deea17b677c99b8e1
chukwuebuka alaegbu <chukwuebukaalaegbu@gmail.com>
2026-01-19 15:19:12 +0100

    looping lists

--- Python/Lists/loopingItems.py
+++ Python/Lists/loopingItems.py
@@ -10,3 +10,23 @@ for i in range(len(fruits)):
 
 for i in fruits:
     print(f"\n{i}")  # Accessing each item directly
+
+#While loop is used when the number of iterations is not known, it continues until a certain condition is met.
+
+newFruits = []
+def removeVowels(myString):
+    vowels = ["a", "e", "i", "o", "u"]
+    for i in vowels:
+        if i in myString:
+            newString = myString.replace(i,"")
+
+
+
+    return newString
+
+for fruit in fruits:
+    removeVowels(fruit)
+
+
+
+print(fruits)
