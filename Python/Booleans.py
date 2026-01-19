c5c95600f0fa7968b71eb4487300861fd2172989
chukwuebuka alaegbu <chukwuebukaalaegbu@gmail.com>
2026-01-19 11:18:44 +0100

    operators

---
+++ Python/Booleans.py
@@ -0,0 +1,9 @@
+#The boolean value of an empty string, list, set, or dictionary is False.
+empty_string = ""
+empty_list = []
+empty_set = set()
+empty_dict = {}
+print(bool(empty_string))  # Output: False
+print(bool(empty_list))    # Output: False
+print(bool(empty_set))     # Output: False
+print(bool(empty_dict))    # Output: False
