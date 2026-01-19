683e618b4918f615b2fe4d372f1364289f93bdc9
chukwuebuka alaegbu <chukwuebukaalaegbu@gmail.com>
2026-01-19 18:10:03 +0100

    sorting

---
+++ Python/Lists/sortingLists.py
@@ -0,0 +1,14 @@
+#.sort() is the chief sorter of lists
+
+#.sort() is case sensitive, hence it gives uppercase letters priority.
+#.sort() sorts in ascending order
+#.sort() sort
+
+fruits = ["banana", "apple", "cherry", "date", "orange","kiwi", "mango", 4, 20]
+fruits.sort()
+print(fruits)
+
+#to sort in descending order, we do .sort(reverse = True)
+
+fruits.sort(reverse=True)
+print(fruits) #reverses the sort.
