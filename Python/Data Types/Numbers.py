61424cb93cd1a11a8dc6ff2d2ed7fc53b417cabf
chukwuebuka alaegbu <chukwuebukaalaegbu@gmail.com>
2026-01-19 09:25:20 +0100

    numbers

---
+++ Python/Data Types/Numbers.py
@@ -0,0 +1,14 @@
+#Int Values
+a = 10
+b = -5
+
+#You can cast float to int
+c = int(3.14)
+print(c)  # Output: 3
+
+#Float Values
+d = 5.75
+e = -2.5
+#You can cast int to float
+f = float(10)
+print(f)  # Output: 10.0
---
+++ Python/Data Types/allDataType.py
@@ -0,0 +1,38 @@
+#String
+
+name = "John Doe"
+
+#Numbers
+age = int (30)
+height = float (5.9)
+complex_number = complex (2, 3)
+
+#boolean
+is_student = False
+is_employed = True
+
+#List
+fruits = ["apple", "banana", "cherry"]
+
+#tuple
+cars = ("Ford", "BMW", "Toyota")
+
+#dictionary
+person = {
+    "name": "Alice",
+    "age": 25,
+    "city": "New York"
+}
+
+#Set ==> Contains only unique values
+unique_numbers = {1, 2, 3, 4, 3, 5}
+unique_person = {"Tommyzan", "Mordy", "Wisdom","Great", "Mordy"}
+
+print(unique_person)
+
+#NoneType
+data = None
+
+#byte
+x = memoryview(bytes(5))
+#print(x)
