d9c1e04503e04ed0bf2e00430cc5298159398518
chukwuebuka alaegbu <chukwuebukaalaegbu@gmail.com>
2026-01-19 10:31:03 +0100

    string slicing

---
+++ Python/Strings/stringSlicing.py
@@ -0,0 +1,7 @@
+# String slicing is the cutting of parts of strings
+# Syntax: string[start:end:step]
+
+value  = "Plasmodium"
+
+#getting the first 4 characters
+print(value[:5])  #Output: Data
--- Python/Strings/strings.py
+++ Python/Strings/strings.py
@@ -5,3 +5,17 @@ sdvfsdfvdfvsdfvdsfvdfv
 vsdfvsdfvsdfvdsfvsdfvsdfsdfvfvsdvfd
 """
 print(multiline_string)
+
+#N/B A string is also an array
+
+name =  "Data Engineering"
+print(name[-1])  #prints last character i.e g
+for i in name:
+     print(i, end=" * ")
+
+#String's length
+print("\nLength of the string is:", len(name))
+
+#checking if a string is within a string  ==> use "in" or "not in" appropriately
+if "Data" in name:
+    print("Yes, 'Data' is present in the string.")
