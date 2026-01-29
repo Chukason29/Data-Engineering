faa6e67aacbd728053c3595457222ba12656e9f5
chukwuebuka alaegbu <chukwuebukaalaegbu@gmail.com>
2026-01-29 10:57:08 +0100

    python math

---
+++ Python/pythonMaths.py
@@ -0,0 +1,18 @@
+import math
+
+print (dir(math)) # shows all the functions in the math module
+
+#.abs() ==> returns absolute value
+print(abs(-75)) #outputs 75
+
+#.min() ==> gets the minimum number
+#.max() ==> gets the maximum number
+#.pow(x, y) ==> return the value of X raised to the power of Y
+print(pow(2, 3)) #outputs 8
+
+#math.ceil rounds numbers upward to the nearest integer.
+print(math.ceil(1.45)) #outputs 2
+
+#math.floor rounds numbers downward to the nearest integer
+print(math.floor(1.45)) #outputs 1
+
